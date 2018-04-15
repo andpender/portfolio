a7c841a650d314ccabfd1988330a8b0e9d5e56fb
Andrew Pender <and.pender@gmail.com>
2018-04-15 16:01:38 +1000

    Adding Crypto portion of site along with coin images

--- public/javascripts/coin.js
+++ public/javascripts/coin.js
@@ -1,11 +1,6 @@
+// Fetch data from API
 function getCoins() {
-    $.getJSON("https://api.coinmarketcap.com/v1/ticker/?convert=AUD", function(data) {
-
-        addData(data);
-    });
-}
-
-fetch("https://api.coinmarketcap.com/v1/ticker/?convert=AUD")
+    fetch("https://api.coinmarketcap.com/v1/ticker/?limit=50")
     .then(
         function(response){
             if (response.status !== 200){
@@ -15,19 +10,25 @@ fetch("https://api.coinmarketcap.com/v1/ticker/?convert=AUD")
 
             // Examine the text in the response
             response.json().then(function(data){
-                console.log(data);
+                addData(data);
             });            
         }
     )
     .catch(function(err) {
         console.log('Fetch Error :-S', err);
     });
+}
 
-
-// This function adds response from coin market cap API to the grid
+// This function adds response from CMC API to the grid
 function addData(data) {
     data.forEach(element => {
+        var ticker = element['symbol'].toLowerCase();
         var coinInfo = document.getElementById('coinInfo');
+        var coinImg = document.createElement('img');
+        coinImg.src = '/images/icons/' + ticker + '.png'
+        
+        coinInfo.insertAdjacentHTML('beforeend', "<div>" + element['rank'] + "</div>");
+        coinInfo.appendChild(coinImg);        
         coinInfo.insertAdjacentHTML('beforeend', "<div>" + element['name'] + "</div>");
         coinInfo.insertAdjacentHTML('beforeend', "<div>$ " + parseFloat(element['price_usd']).toFixed(3).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
         + "</div>");
@@ -41,7 +42,7 @@ function format(n) {
     with (Math) {
         var base = floor(log(abs(n))/log(1000));
         var suffix = 'kmb'[base-1];
-        return suffix ? String(n/pow(1000,base)).substring(0,3)+suffix : ''+n;
+        return suffix ? String(n/pow(1000,base)).substring(0,5)+' '+suffix : ''+n;
     }
 }
 
