76b5c93c0ace6728376b4219bc118e357d6409d3
Andrew Pender <and.pender@gmail.com>
2018-05-22 20:45:20 +1000

    sequelize queries transitioned succesfully

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,22 +9,10 @@ exports.blog_home_get = function(req, res) {
     }).then(rows => {
         res.render('blog_home',{title:"Recent Blog Posts", data:rows, user:req.user});
     })
-    // var sql = 'SELECT * FROM blog.posts LIMIT 50';
-    // var sess = req.session;
-    // req.session.userId = 1;
-    //
-	// req.getConnection(function(err,connection){
-	// 	var query = connection.query(sql,function(err,rows)
-	// 	{
-	// 	    console.log(req.user);
-	// 		res.render('blog_home',{title:"Recent Blog Posts", data:rows, user:req.user});
-	// 	});
-     // });
 };
 
 // Display individual blog page
 exports.blog_page_get = function(req, res) {
-
     Posted.find({
         where: {
             slug: req.params.slug
@@ -34,25 +22,6 @@ exports.blog_page_get = function(req, res) {
     }).catch(err => {
         res.render('blog_page',{title:"Blog Posts",error:"Error Not Found"})
     })
-
-    // models.sequelize.query('SELECT * FROM blog.posts WHERE slug="' + req.params.slug + '"')
-    //     .then(rows => {
-    //         console.log(rows);
-    //         res.render('blog_page',{title:"Blog Posts",data:rows[0], user:req.user});
-    //     })
-
-    // var sql = 'SELECT * FROM blog.posts WHERE slug="' + req.params.slug + '"';
-	// req.getConnection(function(err,connection){
-	// 	var query = connection.query(sql,function(err,rows)
-	// 	{
-     //        if(err)
-     //        {
-     //            res.render('blog_page',{title:"Blog Posts",error:"Error Not Found"})
-     //        }else{
-     //        res.render('blog_page',{title:"Blog Posts",data:rows, user:req.user});
-     //        }
-	// 	});
-     // });
 };  
 
 // GET blog create page
@@ -66,21 +35,6 @@ exports.blog_page_post = function(req, res) {
         var trimmedPath = '';
     } else {
         var trimmedPath = req.file.path.replace("public/uploads", "/uploads");
-    }
-
-    var blog = {
-        title: req.body.title,
-        author: req.body.author,
-        information: req.body.content,
-        slug: slug(req.body.title, {lower: true}),
-        path: trimmedPath,
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-
-    var insert_sql = "INSERT INTO blog.posts SET ?";
-    if (!blog.slug) {
-        insert_sql = '';
     }
 
     Posted.create({
@@ -102,24 +56,6 @@ exports.blog_page_post = function(req, res) {
                     information: req.body.content,
                 });
     });
-
-    // req.getConnection(function(err,connection){
-    //     var query = connection.query(insert_sql, blog, function(err, result){
-    //         if(err || !blog.slug)
-    //         {
-    //             res.render('blog_create',
-    //             {
-    //                 title: req.body.title,
-    //                 author: req.body.author,
-    //                 information: req.body.content
-    //             });
-    //         }else{
-    //             console.log(blog);
-    //             res.redirect('/blog')
-    //         }
-    //     });
-    // });
-
 };
 
 // GET blog edit page
@@ -129,57 +65,41 @@ exports.blog_page_edit_get = function(req, res, next) {
             slug: req.params.slug
         }
     }).then(rows => {
+        console.log(rows);
         res.render('blog_edit',{title:"Blog Posts",data:rows.dataValues, user:req.user});
     }).catch(err => {
         res.render('blog_page',{title:"Blog Posts",error:"Error Not Found"})
     })
-    //
-    // var sql = 'SELECT * FROM blog.posts WHERE slug = "' + req.params.slug + '"';
-    //
-	// req.getConnection(function(err,connection){
-	// 	var query = connection.query(sql,function(err,rows){
-     //        if(err){ return next(err); }
-    //
-     //        else {
-     //            res.render('blog_edit', {title: "Recent Blog Posts", data: rows, user:req.user});
-     //        }
-	// 	});
-     // });
-
-}
+};
 
 // POST blog edit page
 exports.blog_page_edit_post = function(req, res, next) {
-
-    var sql = 'UPDATE blog.posts SET \
-                author = "' + req.body.author + '", \
-                title = "' + req.body.title + '", \
-                information = "' + req.body.content + '", \
-                slug = "' + slug(req.body.title, {lower: true}) + '" \
-                WHERE slug = "' + req.params.slug + '"';
-    req.getConnection(function(err,connection){
-        var query = connection.query(sql, function(err, result){
-            if(err){ return next(err); }
-
-            else{
-                res.redirect('/blog')
-            }		
-        });
-    });
-}
+    Posted.update({
+        title: req.body.title,
+        author: req.body.author,
+        information: req.body.content,
+        slug: slug(req.body.title, {lower: true})
+    },
+        {where: {slug: req.params.slug}}
+    ).then(rowsUpdated => {
+        console.log(rowsUpdated);
+        res.redirect('/blog');
+    }).catch(err => {
+        console.log(err);
+    })
+};
 
 // GET blog delete page
 exports.blog_page_delete_get = function(req, res, next) {
+    Posted.destroy({
+        where: {
+            slug: req.params.slug
+        }
+    }).then(rowDeleted => {
+        console.log(rowDeleted);
+        res.redirect('/blog');
+    }).catch(err => {
+        console.log(err);
+    })
 
-    var sql = 'DELETE FROM blog.posts \
-                WHERE slug = "' + req.params.slug + '"';
-    req.getConnection(function(err,connection){
-        var query = connection.query(sql, function(err, result){
-            if(err){ return next(err); }
-
-            else{
-                res.redirect('/blog')
-            }		
-        });
-    });
 }
